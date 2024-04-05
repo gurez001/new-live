6b1a9d64f9251835b16178accf4a1b0257e1234c
pawan <ps9502748@gmail.com>
2024-04-05 12:28:57 +0530

    url

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ var corsOptions = {
 app.use(
   cors({
     // origin: 'http://localhost:3000', // Allow requests only from this origin
-    origin:[ "http://localhost:3000",'https://gurez-live.netlify.app'], // Allow requests only from this origin
+    origin:[ "http://localhost:3000",'https://gurez-live.netlify.app','https://versel-frontend-tqlk.vercel.app'], // Allow requests only from this origin
     Headers: true,
     exposedHeaders: "Set-Cookie",
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"], // Allow only specified HTTP methods
