e0ed0c8e978eb2b5778bacdea120266c8bf7046f
pawan <ps9502748@gmail.com>
2024-04-05 11:35:04 +0530

    url

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,12 +9,10 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
            path: "/", // cookie path
-          // // expires: new Date('2024-12-31'), // absolute expiration date
-          // // maxAge: 3600, // relative max age of the cookie from when the client receives it in seconds
-          domain: "localhost", // domain for the cookie
-          secure: false, // accessible through HTTP
-          httpOnly: false, // only server can access the cookie
-          sameSite: "lax", // enforcement type
+        //   Domain: ".onrender.com", // domain for the cookie
+          secure: true, // accessible through HTTP
+          httpOnly: true, // only server can access the cookie
+          sameSite: "none", // enforcement type
           partitioned: false, // store using partitioned storage
         };
     res.status(statusCode).cookie('token',token,option).json({
